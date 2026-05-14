ec1b5f4d3a43cc84b2ae41affafcc8a26e57a5ad
Banh <tommyngo156@gmail.com>
2026-05-14 00:47:38 -0400

    feat: Add bookmark url to database

---
+++ script.js
@@ -0,0 +1,22 @@
+const storeBookmark = async (req, res) => {
+  event.preventDefault();
+  const bookmarkUrl = document.getElementById('bookmarkInput').value;
+  const {url , tag, created_at} = {
+    url: bookmarkUrl,
+    tag: "",
+    created_at: new Date().toISOString()
+  };
+  try {
+    const response = await fetch('/bookmarks', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({url, tag, created_at})
+      })
+    } catch (error) {
+      console.error("Error storing bookmark:", error);
+  }
+}
+
+document.getElementById('bookmarkForm').addEventListener('submit', storeBookmark);
