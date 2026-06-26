2ffa135fa74a2ec68e8dac9437192c2f50859914
Banh <tommyngo156@gmail.com>
2026-06-26 00:46:18 -0400

    refactor: Add knex migration to create schemas

---
+++ migrations/20260626040133_create_user_table.js
@@ -0,0 +1,24 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function(knex) {
+  return knex.schema.createTable("users", (table) => {
+    table.increments("id").primary();
+    table.string("username").notNullable().unique();
+    table.string("password").notNullable().checkLength(">=", 8).checkLength("<=", 64);
+    table.timestamp("created_at", { useTz: true }).notNullable().defaultTo(knex.fn.now());
+  }).then(() => {
+    console.log("Users table created successfully");
+});
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function(knex) {
+    return knex.schema.dropTableIfExists("Users").then(() => {
+        console.log("Users table dropped successfully");
+   });
+};
---
+++ migrations/20260626043925_create_bookmark_table.js
@@ -0,0 +1,25 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function(knex) {
+  return knex.schema.createTable("bookmarks", (table) => {
+    table.increments("id").primary();
+    table.string("url").notNullable().unique();
+    table.string("tag").notNullable();
+    table.integer("user_id").unsigned().notNullable();
+    table.timestamp("created_at", { useTz: true }).notNullable().defaultTo(knex.fn.now());
+  }).then(() => {
+    console.log("Bookmarks table created successfully");
+  });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function(knex) {
+  return knex.schema.dropTableIfExists("bookmarks").then(() => {
+    console.log("Bookmarks table dropped successfully");
+  });
+};
